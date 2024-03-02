971289349e217420f82186d4d1988726d8ee8d50
Deliaaan <delian.ngtmr@gmail.com>
2024-03-02 15:02:40 -0600

    "Endpoints get, create, update, delete"

--- src/main/java/com/deliaaan/contactlistApi/controller/ContactController.java
+++ src/main/java/com/deliaaan/contactlistApi/controller/ContactController.java
@@ -3,9 +3,10 @@ package com.deliaaan.contactlistApi.controller;
 import com.deliaaan.contactlistApi.entites.ContactEntity;
 import com.deliaaan.contactlistApi.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RequestMapping("/api/contacts")
 @RestController
@@ -17,4 +18,42 @@ public class ContactController {
     Iterable<ContactEntity> list(){
         return contactRepository.findAll();
     }
+
+    @GetMapping("{id}")
+    public ContactEntity get(@PathVariable Integer id){
+        return contactRepository
+                .findById(id)
+                .orElse(null);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ContactEntity create(@RequestBody ContactEntity contact){
+        contact.setCreatedAt(LocalDateTime.now());
+        return contactRepository.save(contact);
+    }
+
+
+    @PutMapping("{id}")
+    public ContactEntity update(@PathVariable Integer id, @RequestBody ContactEntity contact){
+        ContactEntity contactFromDB = contactRepository
+                .findById(id)
+                .orElse(null);
+
+        contactFromDB.setName(contact.getName());
+        contactFromDB.setEmail(contact.getEmail());
+
+        return contactRepository.save(contactFromDB);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Integer id){
+        ContactEntity contact = contactRepository
+                .findById(id)
+                .orElse(null);
+        contactRepository.delete(contact);
+    }
+
+
 }
